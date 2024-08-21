17c4eab88cb197495c009649fe48b757ef657561
Sam Wlody <swlody@gmail.com>
2024-08-21 16:05:50 -0400

    revert fragile partition tolerance

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -15,8 +15,7 @@ enum RequestPayload {
     Broadcast { message: u64 },
     Read,
     Topology { topology: BTreeMap<NodeId, Vec<NodeId>> },
-    Gossip { messages: BTreeSet<u64> },
-    GossipAck { messages: BTreeSet<u64> },
+    Gossip { message: u64 },
 }
 
 #[allow(clippy::enum_variant_names)]
@@ -26,25 +25,17 @@ enum ResponsePayload<'a> {
     BroadcastOk,
     ReadOk { messages: &'a BTreeSet<u64> },
     TopologyOk,
-    // TODO avoid copying types that are both sent and received
-    Gossip { messages: &'a BTreeSet<u64> },
-    GossipAck { messages: &'a BTreeSet<u64> },
+    Gossip { message: u64 },
 }
 
 struct BroadcastHandler {
     node: Node,
     seen_messages: RefCell<BTreeSet<u64>>,
     neighbors: OnceCell<Vec<NodeId>>,
-    unacked_messages: RefCell<BTreeMap<NodeId, BTreeSet<u64>>>,
 }
 
 impl BroadcastHandler {
-    fn gossip(
-        &self,
-        messages: &BTreeSet<u64>,
-        source: Option<NodeId>,
-    ) -> Result<(), MaelstromError> {
-        let mut unacked_messages = self.unacked_messages.borrow_mut();
+    fn gossip(&self, message: u64) -> Result<(), MaelstromError> {
         for &neighbor in self
             .neighbors
             .get()
@@ -54,16 +45,8 @@ impl BroadcastHandler {
                 )
             })?
             .iter()
-            // Don't gossip back to whichever node we received the gossip message from
-            .filter(|&&n| match source {
-                Some(source) => source != n,
-                None => true,
-            })
         {
-            let neighbor_unacked = unacked_messages.get_mut(&neighbor).unwrap();
-            neighbor_unacked.extend(messages);
-
-            self.node.send(neighbor, ResponsePayload::Gossip { messages: neighbor_unacked })?;
+            self.node.send(neighbor, ResponsePayload::Gossip { message })?;
         }
         Ok(())
     }
@@ -71,17 +54,7 @@ impl BroadcastHandler {
 
 impl Handler<RequestPayload> for BroadcastHandler {
     fn init(node: Node) -> Self {
-        let map = node
-            .network_ids
-            .iter()
-            .map(|&id| (id, BTreeSet::new()))
-            .collect::<BTreeMap<NodeId, BTreeSet<u64>>>();
-        Self {
-            node,
-            seen_messages: RefCell::new(BTreeSet::new()),
-            neighbors: OnceCell::new(),
-            unacked_messages: RefCell::new(map.clone()),
-        }
+        Self { node, seen_messages: RefCell::new(BTreeSet::new()), neighbors: OnceCell::new() }
     }
 
     fn handle(
@@ -92,22 +65,12 @@ impl Handler<RequestPayload> for BroadcastHandler {
             RequestPayload::Broadcast { message } => {
                 self.seen_messages.borrow_mut().insert(*message);
                 self.node.reply(&broadcast_msg, ResponsePayload::BroadcastOk)?;
-                self.gossip(&BTreeSet::from([*message]), None)?;
+                self.gossip(*message)?;
             }
-            RequestPayload::Gossip { messages } => {
-                self.node.reply(&broadcast_msg, ResponsePayload::GossipAck { messages })?;
-                let mut seen_messages = self.seen_messages.borrow_mut();
-                let new_messages =
-                    messages.difference(&seen_messages).copied().collect::<BTreeSet<u64>>();
-                seen_messages.extend(new_messages.clone());
-                self.gossip(&new_messages, Some(broadcast_msg.src))?;
-            }
-            RequestPayload::GossipAck { messages } => {
-                self.unacked_messages
-                    .borrow_mut()
-                    .get_mut(&broadcast_msg.src)
-                    .unwrap()
-                    .retain(|m| !messages.contains(m));
+            RequestPayload::Gossip { message } => {
+                if self.seen_messages.borrow_mut().insert(*message) {
+                    self.gossip(*message)?;
+                }
             }
             RequestPayload::Read => {
                 self.node.reply(
